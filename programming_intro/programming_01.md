af3c0c98cf2b13303a5493e21750bd2527048936
arielchuri <github@sparklelabs.com>
2022-02-14 12:08:28 -0500

    programming intro

---
+++ programming_intro/programming_01.md
@@ -0,0 +1,38 @@
+Programming Intro
+
+# What is programming
+# The development environment
+
+# Section 1
+
+## The computers "memory"
+
+## Numbers and basic operations
+
+## Strings
+
+## Creating variables
+
+## Lists
+
+## If
+
+## Else
+
+# Section 2
+
+## Ints, floats, and types of variables
+
+## Variables in strings
+
+## Booleans
+
+## Comparisons
+
+## Lists - add, insert, delete
+
+## Loops
+
+## Ranges
+
+## Functions
---
+++ programming_intro/programming_01.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+print('Hello world')
+
+# Section 1
+
+## The computers "memory"
+
+## Numbers and basic operations
+print(2+1)
+
+## Strings
+print('Hello world')
+
+## Creating variables
+my_first_var = 4
+
+my_second_var = 2+1
+
+print(my_first_var, my_second_var)
+
+## Lists
+x = [ 1, 2, 3, 4]
+
+print(x)
+
+print(x[1])
+
+print( x[0] + x[3])
+
+## If
+if True: print('goodbye')
+
+if x[0] == 1:
+    print('x[0] equals 1')
+
+## Else
+if x[0] == 2:
+    print('x[0] equals 2')
+else:
+    print('x[0] != 2')
+
+
+# Section 2
+
+## Ints, floats, and types of variables
+y = 1.24
+
+print(type(y))
+print(type(x))
+print(type(x[2]))
+
+
+## Variables in strings
+print (f'y = {y}')
+print (f'x[1] = {x[1]}')
+
+## Booleans
+z = False
+
+print(z)
+
+z = not z
+
+print(z)
+
+print(not z)
+
+## Comparisons
+if z:
+    print('yes')
+
+## Lists - add, insert, delete
+
+
+## Loops
+
+## Ranges
+
+## Functions
